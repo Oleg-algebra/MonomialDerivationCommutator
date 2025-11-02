776af85a18ed0e2d5ed8c6fcc782cc5ab4344803
Oleg Tyshchenko <sokol.wise@gmail.com>
2025-11-02 20:38:35 +0200

    1) minor fixes, removing usage of Monomial class in ConstantSearch

--- basicClasses/constantSearchSymbolic.py
+++ basicClasses/constantSearchSymbolic.py
@@ -26,25 +26,38 @@ class Monomial:
 
 
 
+
+
 class Polynomial:
-    def __init__(self, coefficients = None,n_var: int = None,poly_symbols = None,vars = None):
+    def __init__(self, coefficients: np.ndarray = None, n_var: int = None, poly_symbols = None, variables = None):
 
         self.polynomial_symbolic = 0
         self.coefficients = coefficients
         if poly_symbols is not None:
             self.polynomial_symbolic = poly_symbols
         self.variables_polynom = []
-        if vars is not None:
-            self.variables_polynom = vars
+        if variables is not None:
+            self.variables_polynom = variables
+        else:
+            self.variables_polynom = [symbols(f"x_{i}") for i in range(n_var)]
         if coefficients is not None:
             for i in range(coefficients.shape[0]):
                 for j in range(coefficients.shape[1]):
-                    monomial = Monomial(n_var,coefficients[i,j],[i,j])
-                    if self.variables_polynom == []:
-                        self.variables_polynom = monomial.vars
-                    self.polynomial_symbolic += monomial.monomial_symbolic
-
-
+                    self.polynomial_symbolic += coefficients[i,j]*self.variables_polynom[0]**i*self.variables_polynom[1]**j
+        else:
+            row_max = 0
+            column_max = 0
+            poly = Poly(poly_symbols, variables)
+            for term in poly.terms():
+                degree = term[0]
+                if degree[0]> row_max:
+                    row_max = degree[0]
+                if degree[1]> column_max:
+                    column_max = degree[1]
+            self.coefficients = np.zeros((row_max+1,column_max+1))
+            for term in poly.terms():
+                degree = term[0]
+                self.coefficients[degree[0]][degree[1]] = term[1]
 
 class Derivation:
     def __init__(self, polynomials: list[Polynomial], variables):
@@ -69,8 +82,7 @@ class ConstantSearchSymbolic:
         self.strategy = strategy
         self.unknown_constant = self.generatePolynomial()
         self.searchCommutator = {
-            "general" : self.generalSolver,
-            "linear" : self.linearSolver
+            "general" : self.generalSolver
         }
 
 
@@ -111,7 +123,7 @@ class ConstantSearchSymbolic:
                 self.unknown_coeffients.append(coef)
             matrix.append(row)
         matrix = Matrix(matrix)
-        return Polynomial(matrix,len(variables))
+        return Polynomial(matrix,len(variables),variables=variables)
 
     def generalSolver(self):
 
@@ -130,48 +142,7 @@ class ConstantSearchSymbolic:
 
         return res
 
-    def linearSolver(self):
 
-
-        derivatives1 = []
-        for poly in self.unknown_constant.polynomials:
-            derivatives1.append(self.derivation.take_derivative(poly.polynomial_symbolic))
-
-        derivatives2 = []
-        for poly in self.derivation.polynomials:
-            derivatives2.append(self.unknown_constant.take_derivative(poly.polynomial_symbolic))
-
-        polys = []
-        for i in range(len(derivatives1)):
-            polys.append(derivatives1[i] - derivatives2[i])
-
-        equations = []
-        variables = self.unknown_constant.variables
-
-        terms = []
-        for poly in polys:
-            p = Poly(poly, variables)
-            for coeff in p.terms():
-                p1  = Poly(coeff[1],self.unknown_coeffients)
-                terms.append(p1)
-
-        for term in terms:
-            # print(term)
-            row = np.zeros((1,len(self.unknown_coeffients)))
-            for t in term.terms():
-                coeffs = np.array(t[0])
-                scalar = float(N(t[1], chop=True))
-                row = row + coeffs * scalar
-            equations.append(row[0])
-
-        # print(f'equations: {equations}')
-        matrix = np.array(equations)
-        # print(matrix)
-        matrix = np.concatenate((matrix,np.zeros((matrix.shape[0],1))),axis=1)
-        matrix = Matrix(matrix)
-        res = solve_linear_system(matrix, *self.unknown_coeffients)
-
-        return res
 
     def get_constant(self, solver ="general"):
 
